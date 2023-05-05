da10b57fd17eb8489426f04bca0f776ab5180eb6
Ahmed yehia <yehiaahmed900@gmail.com>
2023-05-05 15:03:01 +0200

    finished AVL except delete node

---
+++ AVL.cpp
@@ -0,0 +1,138 @@
+#include "AVL.h"
+using namespace std;
+template<class T>
+int AVL<T>::get_height(AVL_node<T> *node) {
+    int left = 0 , right = 0;
+    if(node->left != nullptr){
+        left = node->left->height;
+    }
+    if(node->right != nullptr){
+        right = node->right->height;
+    }
+    return  1 + max(left , right);
+}
+template<class T>
+int AVL<T>::getBalanceFactor(AVL_node<T> *node) {
+    int left = 0 , right = 0;
+    if(node->left != nullptr){
+        left = node->left->height;
+    }
+    if(node->right != nullptr){
+        right = node->right->height;
+    }
+    return left - right;
+}
+template<class T>
+void AVL<T>::right_rotate(AVL_node<T> *node) {
+    AVL_node<T>* temp = node->left;
+    node->left = temp->right;
+    if(temp->right != nullptr){
+        temp->right->parent = node;
+    }
+    temp->parent = node->parent;
+    if(node->parent == nullptr){
+        this->root = temp;
+    }
+    else if(node == node->parent->left){
+        node->parent->left = temp;
+    }
+    else{
+        node->parent->right = temp;
+    }
+    temp->right = node;
+    node->parent = temp;
+    node->height = get_height(node);
+    temp->height = get_height(temp);
+    node->bf = getBalanceFactor(node);
+    temp->bf = getBalanceFactor(temp);
+
+}
+template<class T>
+void AVL<T>::left_rotate(AVL_node<T> *node) {
+    AVL_node<T>* temp = node->right;
+    node->right = temp->left;
+    if(temp->left != nullptr){
+        temp->left->parent = node;
+    }
+    temp->parent = node->parent;
+    if(node->parent == nullptr){
+        this->root = temp;
+    }
+    else if(node == node->parent->left){
+        node->parent->left = temp;
+    }
+    else{
+        node->parent->right = temp;
+    }
+    temp->left = node;
+    node->parent = temp;
+    node->height = get_height(node);
+    temp->height = get_height(temp);
+    node->bf = getBalanceFactor(node);
+    temp->bf = getBalanceFactor(temp);
+}
+template<class T>
+void AVL<T>::insert(T key) {
+    AVL_node<T> *node = new AVL_node<T>(key);
+    if (this->root == nullptr) {
+        this->root = node;
+        return;
+    }
+    AVL_node<T> *temp = this->root;
+    while (true) {
+        if (key < temp->key) {
+            if (temp->left == nullptr) {
+                temp->left = node;
+                node->parent = temp;
+                break;
+            }
+            temp = temp->left;
+        } else {
+            if (temp->right == nullptr) {
+                temp->right = node;
+                node->parent = temp;
+                break;
+            }
+            temp = temp->right;
+        }
+    }
+    while(temp != nullptr){
+        temp->height = get_height(temp);
+        temp->bf = getBalanceFactor(temp);
+        temp = temp->parent;
+    }
+    temp = node->parent;
+    while(temp!= nullptr){
+        if(temp->bf < -1 && temp->right->bf < 0){
+            left_rotate(temp);
+        }
+        else if(temp->bf < -1 && temp->right->bf > 0){
+            right_rotate(temp->right);
+            left_rotate(temp);
+        }
+        else if(temp->bf > 1 && temp->left->bf > 0){
+            right_rotate(temp);
+        }
+        else if(temp->bf > 1 && temp->left->bf < 0){
+            left_rotate(temp->left);
+            right_rotate(temp);
+        }
+        temp = temp->parent;
+    }}
+template<class T>
+void AVL<T>::print() {
+    queue<AVL_node<T>*> q;
+    q.push(this->root);
+    while(!q.empty()){
+        AVL_node<T>* temp = q.front();
+        q.pop();
+        cout << temp->key << " ";
+        if(temp->left != nullptr){
+            q.push(temp->left);
+        }
+        if(temp->right != nullptr){
+            q.push(temp->right);
+        }
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "AVL.cpp"
 using namespace std;
 
 int main(){
+    AVL<int> tree;
+    tree.insert(10);
+    tree.insert(20);
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(50);
+    tree.insert(25);
+    tree.insert(35);
+    tree.print();
 
 }
