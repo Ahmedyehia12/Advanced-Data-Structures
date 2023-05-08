31dd0c70e7cdb71125bcfec2ab5fa156b21c9ed9
Ahmed yehia <yehiaahmed900@gmail.com>
2023-05-08 17:43:04 +0200

    bla bla

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include "BST.cpp"
 using namespace std;
 
 int main(){
-    BST<int> bst;
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(7);
-    bst.insert(2);
-    bst.insert(4);
-    bst.insert(6);
-    bst.deleteNode(5);
-    bst.print(bst.getRoot());
 
 }
